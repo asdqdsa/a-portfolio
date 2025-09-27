7bc3a31af96fddbf18a4101e1ec3eb9a79baab9d
asdqdsa <asdqdsa@protonmail.com>
2025-09-27 12:50:34 +0400

    chore: add styline idiomatic order

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,6 +1,9 @@
 export default {
   customSyntax: 'postcss-scss',
-  extends: ['stylelint-config-recommended-scss'],
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-idiomatic-order',
+  ],
   plugins: ['stylelint-scss'],
   rules: {
     'nesting-selector-no-missing-scoping-root': null,
