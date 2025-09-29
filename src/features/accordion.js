ad9fe4f36eb24291fc68b5559fb1c1b2a4b2e57a
asdqdsa <asdqdsa@protonmail.com>
2025-09-30 03:03:09 +0400

    feat: add faq section

--- src/features/accordion.js
+++ src/features/accordion.js
@@ -1,4 +1,15 @@
 document.querySelectorAll('.accordion__header').forEach((el) => {
+  const item = el.closest('.accordion__item');
+  const icon = item.querySelector('.accordion__icon use');
+
+  /** @type {HTMLElement} content */
+  const content = item.querySelector('.accordion__content');
+
+  if (item.classList.contains('active')) {
+    content.style.maxHeight = content.scrollHeight / 16 + 'rem';
+    icon.setAttribute('href', '/minus.svg#minus');
+  }
+
   el.addEventListener('click', () => {
     const item = el.closest('.accordion__item');
     const icon = item.querySelector('.accordion__icon use');
@@ -8,12 +19,16 @@ document.querySelectorAll('.accordion__header').forEach((el) => {
     item.classList.toggle('active');
 
     if (item.classList.contains('active')) {
+      content.style.maxHeight = content.scrollHeight / 16 + 'rem';
       icon.setAttribute('href', '/minus.svg#minus');
+    }
 
+    if (item.classList.contains('active')) {
       content.style.maxHeight =
         content.scrollHeight === 0
           ? content.scrollHeight + ''
           : content.scrollHeight / 16 + 'rem';
+      icon.setAttribute('href', '/minus.svg#minus');
     } else {
       icon.setAttribute('href', '/plus.svg#plus');
       content.style.maxHeight = '0';
