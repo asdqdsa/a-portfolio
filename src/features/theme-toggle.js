b6db85b07ffcc199d38299891c861b5a6777a939
asdqdsa <asdqdsa@protonmail.com>
2025-09-30 03:33:48 +0400

    refactor: theme toggle

--- src/features/theme-toggle.js
+++ src/features/theme-toggle.js
@@ -4,7 +4,11 @@ const themeBtn = document.querySelector('#logo');
 /** @type {HTMLElement} theme */
 const theme = document.querySelector('#root');
 
+const themes = ['dark', 'light', 'gold'];
+
 themeBtn.addEventListener('click', () => {
   if (!theme) return;
-  theme.dataset.theme = theme.dataset.theme === 'dark' ? 'gold' : 'dark';
+
+  theme.dataset.theme =
+    themes[(themes.indexOf(theme.dataset.theme) + 1) % themes.length];
 });
